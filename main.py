aa0592ec576dc797ce25280169b07caf3241ad2f
GsDrus39 <gleb.gold@bk.ru>
2021-02-21 13:58:42 +0200

    Случайные Окружности

--- main.py
+++ main.py
@@ -2,12 +2,39 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QGridLayout
 from PyQt5.QtGui import QPainter, QPen, QPixmap, QColor
 from random import randint
+from PyQt5 import QtCore, QtWidgets
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(409, 344)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 101, 51))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 409, 20))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Нажми меня"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
@@ -20,11 +47,11 @@ class MyWidget(QMainWindow):
         layout.addWidget(self.label, 1, 0)
 
     def run(self):
-        x, y, w = randint(40, 560), randint(40, 560), randint(10, 40)
+        x, y, w = randint(20, 500), randint(20, 500), randint(10, 40)
         painter = QPainter(self.label.pixmap())
         pen = QPen()
         pen.setWidth(5)
-        pen.setColor(QColor(255, 255, 0))
+        pen.setColor(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         painter.setPen(pen)
         painter.drawEllipse(x, y, w, w)
         painter.end()
